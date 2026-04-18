4c376a367846c3716fd4ec23f9108c419d886fba
Autumn Allison <aal1206@wgu.edu>
2026-04-18 18:36:02 -0500

    Added plant inventory page and enabled access via security config

---
+++ src/main/java/com/verdantroots/pims/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.verdantroots.pims.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/","/plants/**", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}

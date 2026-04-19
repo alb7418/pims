c0028f9fdba78722be27ba6522433a4acabf6b4d
Autumn Allison <aal1206@wgu.edu>
2026-04-18 23:50:03 -0500

    Fixed error duplicating supply edits instead of actually updating

--- src/main/java/com/verdantroots/pims/entity/Supply.java
+++ src/main/java/com/verdantroots/pims/entity/Supply.java
@@ -51,6 +51,10 @@ public class Supply {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getItemName() {
         return itemName;
     }

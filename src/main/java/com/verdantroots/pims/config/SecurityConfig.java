0ca6fc731b36ebdd04dd6ecb296859fdb55f0366
Autumn Allison <aal1206@wgu.edu>
2026-04-18 22:06:25 -0500

    Changed allow all for security during build phase and created supplies.html

--- src/main/java/com/verdantroots/pims/config/SecurityConfig.java
+++ src/main/java/com/verdantroots/pims/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.verdantroots.pims.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/plants/**", "/supplies/**", "/css/**", "/js/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(Customizer.withDefaults())
+                        .anyRequest().permitAll())
                 .csrf(csrf -> csrf.disable());
         return http.build();
     }

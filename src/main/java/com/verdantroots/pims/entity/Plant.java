c2505ad875b54495096fef06cbf48e9e7ef797ad
Autumn Allison <aal1206@wgu.edu>
2026-04-19 14:38:28 -0500

    created local-goods.html, fixed error when editing plant infor (creating new plant instead of editing already made one), updated navigation across all pages, added Low Stock Flagging on all pages

--- src/main/java/com/verdantroots/pims/entity/Plant.java
+++ src/main/java/com/verdantroots/pims/entity/Plant.java
@@ -74,6 +74,10 @@ public class Plant {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getCommonName() {
         return commonName;
     }
